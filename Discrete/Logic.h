604f4067ad1c55dcafc8732acd5b7e0cc14ae2cb
Luke <lr98lr@aol.com>
2016-10-15 23:18:36 -0400

    added logical operators as a tuple

--- Discrete/Logic.h
+++ Discrete/Logic.h
@@ -3,6 +3,12 @@ class Logic{
 public:
 
 
+	static const char and = '^';
+	static const char or = 'V';
+	static const char conditional = '-';
+
+
+
 
 };
 
--- Discrete/Reciever.cpp
+++ Discrete/Reciever.cpp
@@ -20,7 +20,7 @@ void Reciever::decipher(){
 	std::vector<int> positions;
 	std::string tmp = statement.info;
 	while(pos != std::string::npos){
-		if(pos < tmp.length() && tmp[pos] == '^'/*Logic::and*/){
+		if(pos < tmp.length() && tmp[pos] == Logic::and){
 			std::cout << "Found ^ operator\n";
 			if(pos + 1 < tmp.length())
 				tmp = tmp.substr(pos + 1);
@@ -29,7 +29,7 @@ void Reciever::decipher(){
 
 			pos = tmp.find_first_of(searchString);
 		}
-		if(pos < tmp.length() && tmp[pos] == 'v' /*Logic::or*/){
+		if(pos < tmp.length() && tmp[pos] == Logic::or){
 			std::cout << "Found v operator\n";
 			if(pos + 1 < tmp.length())
 				tmp = tmp.substr(pos + 1);
@@ -37,7 +37,7 @@ void Reciever::decipher(){
 				break;
 			pos = tmp.find_first_of(searchString);
 		}
-		if(pos < tmp.length() && tmp[pos] == '-'/*Logic::conditional*/){
+		if(pos < tmp.length() && tmp[pos] == Logic::conditional){
 			std::cout << "Found -> operator\n";
 			if(pos + 1 < tmp.length()){
 				tmp = tmp.substr(pos + 1);
