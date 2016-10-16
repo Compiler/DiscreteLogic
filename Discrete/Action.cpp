012f3c06c39b646ee589527ba2fb3b8c89fac6af
Luke <lr98lr@aol.com>
2016-10-16 02:05:11 -0400

    dynamic array of booleans for logic enabled

--- Discrete/Action.cpp
+++ Discrete/Action.cpp
@@ -7,13 +7,44 @@ Action::Action(){
 
 
 void Action::truthTable(Statement statement){
+
+	
+
 	amountOfOperators = (int)pow(2, statement.variables.size());
+	
 
 	std::cout << "\n\n\tTruth Table\n\n\t";
 	for(int i = 0; i < statement.variables.size(); i++){
+		std::cout <<"___";
+	}
+	std::cout << "\n\t";
+	for(int i = 0; i < statement.variables.size(); i++){
 		std::cout << "| " << statement.variables[i] << " ";
 	}
-	std::cout << "| " << statement.info << " ";
+	std::cout << "| " << statement.info << "\n\t";
+
+
+	bool **tableInfo;
+
+	// Allocate memory
+	tableInfo = new bool*[amountOfOperators];
+	for(int i = 0; i < amountOfOperators; ++i)
+		tableInfo[i] = new bool[statement.variables.size() + 1];
+
+
+
+
+
+
+
+
+
+
+	// De-Allocate memory to prevent memory leak
+	for(int i = 0; i < amountOfOperators; ++i)
+		delete[] tableInfo[i];
+	delete[] tableInfo;
+
 
 }
 
